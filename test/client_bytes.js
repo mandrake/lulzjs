bf6834fddf0a28e9ae66e0478cbcaac011e15ffb
meh <meh.ffff@gmail.com>
2009-02-08 05:30:37 +0100

    Fixed the bytes tests.

--- test/client_bytes.js
+++ test/client_bytes.js
@@ -7,5 +7,5 @@ socket.connect("localhost", 2707);
 
 socket.sendBytes(new Bytes([0x23,0x42,0x66,0x60]));
 var bytes = socket.receiveBytes(2);
-Console.writeLine("Server: "+bytes);
+Console.writeLine("Server: "+bytes.inspect());
 
--- test/server_bytes.js
+++ test/server_bytes.js
@@ -7,6 +7,6 @@ socket.listen(null, 2707);
 
 var client = socket.accept();
 var bytes = client.receiveBytes(4);
-Console.writeLine("Client: "+bytes);
+Console.writeLine("Client: "+bytes.inspect());
 client.sendBytes(new Bytes([0x61,0x00]));
-Console.writeLine("DONE :O "+client);
+Console.writeLine("DONE :O");
