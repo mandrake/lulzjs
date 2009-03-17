3d1469fcd4dff9e7533da5ec7efb92221f8555d1
meh <meh.ffff@gmail.com>
2009-03-17 03:16:15 +0100

    Maybe it's fixed.

--- js/install.sh
+++ js/install.sh
@@ -8,8 +8,18 @@ function bawww {
     exit 1
 }
 
-make install || bawww
-chmod    a+x  /usr/include/lulzjs
-chmod -R a+r  /usr/include/lulzjs
-chmod    a+rx /usr/lib/liblulzjs.so
+# headers
+mkdir -p /usr/include/lulzjs || bawww
+cp -rf *.h /usr/include/lulzjs || bawww
+chmod    a+x  /usr/include/lulzjs || bawww
+chmod -R a+r  /usr/include/lulzjs || bawww
 
+# library
+cp -f liblulzjs.so /usr/lib/ || bawww
+chmod  a+rx /usr/lib/liblulzjs.so || bawww
+
+# config
+cp -f js-config /usr/bin/lulzjs-config || bawww
+chmod a+xr /usr/bin/lulzjs-config || bawww
+
+echo "Installed tracemonkey without any problem <3"
